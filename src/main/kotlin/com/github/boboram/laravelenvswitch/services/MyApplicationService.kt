2634bf056618180aa275788cc2ff809efbafa215
GitHub Action <action@github.com>
2022-04-23 02:33:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/boboram/laravelenvswitch/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/boboram/laravelenvswitch/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.boboram.laravelenvswitch.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.boboram.laravelenvswitch.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/boboram/laravelenvswitch/services/MyProjectService.kt
+++ src/main/kotlin/com/github/boboram/laravelenvswitch/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.boboram.laravelenvswitch.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.boboram.laravelenvswitch.MyBundle
 
 class MyProjectService(project: Project) {
 
